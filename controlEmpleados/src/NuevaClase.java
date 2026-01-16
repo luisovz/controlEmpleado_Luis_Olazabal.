95d49d33dd2758f67603c358aac01e4ab1d0df4f
luisovz <luis.olaaazabal@gmail.com>
2026-01-16 15:14:18 +0100

    creada nuevaclase.java en rama

--- controlEmpleados/src/NuevaClase.java
+++ controlEmpleados/src/NuevaClase.java
@@ -1,6 +1,6 @@
 
 public class NuevaClase {
 public static void main(String[] args) {
-	System.out.println("en la rama. Se crea la clase nueva");
+	System.out.println("een la rama. Se crea la clase nueva");
 }
 }
