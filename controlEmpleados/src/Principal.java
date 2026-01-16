0a7368d51ab9f8287feffb291dd23e4a5b7321cd
luisovz <luis.olaaazabal@gmail.com>
2026-01-16 15:03:27 +0100

    lineas añadidas

--- controlEmpleados/src/Principal.java
+++ controlEmpleados/src/Principal.java
@@ -18,7 +18,8 @@ public class Principal extends JFrame implements ActionListener{
     private JButton b1,b2,b3,b4; 
       
 	public Principal() {
-	
+System.out.println("bienvenido");
+System.out.println("segunda adios");
 	 setTitle("Ventana INICIAL.");
      setLayout(null);
 	 setSize(450, 250);
@@ -48,7 +49,7 @@ public class Principal extends JFrame implements ActionListener{
 	 setVisible(true);
    }
 
-	public void actionPerformed(ActionEvent e) //acción cuando pulsamos botones 
+	public void actionPerformed(ActionEvent e) //acciï¿½n cuando pulsamos botones 
 	    {  	  	
 	    	     if (e.getSource() == b1) 	
 	    	     {	   new VentanaEmple(this);
